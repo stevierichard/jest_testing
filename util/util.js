d2275132302f13a03959835fe33b12ea92bc5d5e
Steven Simon <stevierichard1@gmail.com>
2020-06-03 14:53:05 -0700

    add error test to minus function

--- util/util.js
+++ util/util.js
@@ -1,5 +1,15 @@
 const add = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("2nd argument should be a number");
   return num1 + num2;
 };
 
-module.exports = { add };
+const minus = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("2nd argument should be a number");
+  return num1 - num2;
+};
+
+module.exports = { add, minus };
