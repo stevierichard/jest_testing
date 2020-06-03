603c6bbda9f8036ee07190309d47faa7c0fdb28f
Steven Simon <stevierichard1@gmail.com>
2020-06-03 14:50:51 -0700

    add error test to minus function

--- test/util.test.js
+++ test/util.test.js
@@ -6,14 +6,34 @@ describe("Add", () => {
       expect(add(2, 2)).toBe(4);
     });
   });
-  describe("Failure", () => {
+  describe("Fail", () => {
     it("Should throw an error if 1st argument is not a number", () => {
       expect(() => add("2", 2)).toThrowError();
     });
     it("Should throw an error if 1st argument is not a number", () => {
       expect(() => add(2, "2")).toThrowError();
     });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => add()).toThrowError();
+    });
   });
 });
 
-describe("Minus", () => {});
+describe("Minus", () => {
+  describe("Success", () => {
+    it("Should return first argument minus second", () => {
+      expect(minus(4, 2)).toBe(2);
+    });
+  });
+  describe("Fail", () => {
+    it("Should return an error if the first argument is not a number", () => {
+      expect(() => minus("2", 2)).toThrowError();
+    });
+    it("Should return an error if the second argument is not a number", () => {
+      expect(() => minus(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => minus()).toThrowError();
+    });
+  });
+});
